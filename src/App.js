bac6a4fb5a9027592f9a3e55da1b6e195d8b54d0
fil <fvilca@gmail.com>
2024-09-01 00:35:46 -0500

    adding components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./scss/layout/header.scss";
+import "./scss/layout/header_.scss";
+import "./scss/typography.scss"
+
+import "./App.scss";
+import "./App_.scss";
+import { Route, Routes, useLocation } from "react-router-dom";
+import { useTransition, animated as a, config } from "@react-spring/web";
+import HomePage from "./Home__Page";
+import MenuBar from "./components/MenuBar";
+
+import ManufacturaPage  from "./Manufactura__Page";
+import IngenieriaPage from "./Ingenieria__Page";
+import ProductosPage from "./Productos__Page";
 
 function App() {
+  let location = useLocation();
+
+  const transitions = useTransition(location, {
+    from: { opacity: 0, transform: "translate(-5%,0)" },
+    enter: { opacity: 1, transform: "translate(0%,0)" },
+    leave: { opacity: 0, transform: "translate(10%,0)" },
+    config: {
+      config: config.slow,
+    },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <MenuBar />
+      {transitions((props, item) => {
+        return (
+          <a.div id="main" style={props}>
+            <Routes location={item}>
+              <Route path="/manufactura" element={<ManufacturaPage />} />
+              <Route exact path="/ingenieria" element={<IngenieriaPage/>} />
+              <Route exact path="/productos" element={<ProductosPage/>} />
+              
+              <Route exact path="/" element={<HomePage />} />
+            </Routes>
+          </a.div>
+        );
+      })}
+    </>
   );
 }
 
